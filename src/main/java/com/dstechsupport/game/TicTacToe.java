96d5fb2ea2a3fe14fb3de1bc17cdc1845c02166a
Guðni Garðarsson <gudni@i-66d176ff.greenqloud.internal>
2013-11-09 15:18:44 +0000

    Added directory structure pom.xml and base java files

---
+++ src/main/java/com/dstechsupport/game/TicTacToe.java
@@ -0,0 +1,13 @@
+package com.dstechsupport.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class TicTacToe.java 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
