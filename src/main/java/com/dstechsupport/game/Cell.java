764299a0a06d8dadfb91210977e6b071ffdc0d0a
joinn <johannl12@ru.is>
2013-11-19 11:27:47 +0000

    UpdateCell implemented, test works

--- src/main/java/com/dstechsupport/game/Cell.java
+++ src/main/java/com/dstechsupport/game/Cell.java
@@ -8,13 +8,14 @@ public class Cell
 
 	public Cell( int _column, int _row )
 	{
+	    content = Symbol.EMPTY;
 	    column = _column;
 	    row = _row;
 	}
 
 	public void updateCell( Symbol newSymbol )
 	{
-
+	    content = newSymbol;
 	}
 	
 	public void draw()
