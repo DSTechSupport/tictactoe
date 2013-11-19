32233f25ed72bf817232c7078ce591d710324135
Gudni <gudnig10@ru.is>
2013-11-19 11:07:29 +0000

    added test class for Board

---
+++ src/test/java/com/dstechsupport/game/BoardTest.java
@@ -0,0 +1,17 @@
+/*
+*
+* This test checks if cell does get the correct input and also if a cell is updated once it's been updated
+*
+*
+*/
+
+package com.dstechsupport.game;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public Class BoardTest extends TestCase
+{
+
+}
