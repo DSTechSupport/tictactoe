07533f2f8d83915842c2c7268b198038e904e179
Gudni <gudnig10@ru.is>
2013-11-19 10:03:15 +0000

    updated constructor unit test

--- src/test/java/com/dstechsupport/game/CellTest.java
+++ src/test/java/com/dstechsupport/game/CellTest.java
@@ -5,21 +5,29 @@
 *
 */
 
-import junit.framework.Assert;
+package com.dstechsupport.game;
 
-public class CellTest
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class CellTest extends TestCase
 {
+	@Test
 	public void testCell()
 	{
-	board[1][1] = 'O';
-	Assert.assertEquals('O');
-	}
+	    // create test class
+	    Cell tester = new Cell( 1, 2 );
 
+	    AssertEquals( "column initalized to 1", 1, tester.column );
+	    AssertEquals( "row initalized to 2", 2, tester.row );
+	}
+	
+	@Test
 	public void testUpdateCell()
 	{
-	
-
-
+	    assertTrue( true );
 	}
 
 
