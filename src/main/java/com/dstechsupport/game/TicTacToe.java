d4e39b08d0436520f9a2342c74b17200caca4d08
Gudni <gudnig10@ru.is>
2013-11-20 14:31:19 +0000

    added code from helloworld tutorial for testing

--- src/main/java/com/dstechsupport/game/TicTacToe.java
+++ src/main/java/com/dstechsupport/game/TicTacToe.java
@@ -1,15 +1,37 @@
 package com.dstechsupport.game;
-
+import static spark.Spark.*;
+import spark.*;
 /**
- * Hello world!
+ *
  *
  */
 public class TicTacToe
 {
     public static void main( String[] args )
     {
+	staticFileLocation("/public");
+
+	get(new Route("/hello") 
+	{
+        	@Override
+           	public Object handle(Request request, Response response) {
+                return "Hello World!";
+		}
+        }
+
+	post(new Route("/add") 
+	{
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer a = Integer.valueOf(request.queryParams("a"));
+                Integer b = Integer.valueOf(request.queryParams("b"));
+                return a + b;
+		}
+	}
+
+
         System.out.println( "Welcome to this game of Tic Tac Toe!" );
-   
+
 	Game game = new Game();
 
 	game.play();
