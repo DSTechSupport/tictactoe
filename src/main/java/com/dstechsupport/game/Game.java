9b1dfc9386096800e5e88cc425c609ce7eb93ea7
joinn <johannl12@ru.is>
2013-11-20 09:54:09 +0000

    makeMove implementation started, not finished

--- src/main/java/com/dstechsupport/game/Game.java
+++ src/main/java/com/dstechsupport/game/Game.java
@@ -1,4 +1,6 @@
 package com.dstechsupport.game;
+import java.util.Scanner;
+
 
 public class Game 
 {
@@ -12,17 +14,51 @@ public class Game
 	}
 
 	//The main game loop
-	public void  play()
-
 	public void play()
-
 	{
 	}
 
 	//Take input from player and update the board
 	public void makeMove( Symbol player )
 	{
+		int chosenRow;
+		int chosenColumn;
+		Scanner in = new Scanner(System.in);
 
+		if(player == Symbol.CIRCLE)
+		{
+			System.out.print("CIRCLE´S TURN");
+			do
+			{
+
+			System.out.print("Select row first, column second");
+			chosenRow = in.nextInt();
+			chosenColumn = in.nextInt();
+			chosenRow = chosenRow - 1;
+			chosenColumn = chosenColumn - 1;
+			System.out.println("Row or column input illegal, must be between 1 and 3. Try again.");
+	
+			}while(chosenRow < 0 || chosenRow > 2 || chosenColumn < 0 || chosenColumn > 2);
+		}
+		else // Player symbol is CROSS
+		{
+                if(player == Symbol.CROSS)
+                {
+                        System.out.print("CROSS' TURN");
+                        do
+                        {
+
+                        System.out.print("Select row first, column second");
+                        chosenRow = in.nextInt();
+                        chosenColumn = in.nextInt();
+			chosenRow = chosenRow - 1;
+                        chosenColumn = chosenColumn - 1;
+                        System.out.println("Row or column input illegal, must be between 1 and 3. Try again.");
+
+                        }while(chosenRow < 0 || chosenRow > 2 || chosenColumn < 0 || chosenColumn > 2);
+
+		}
+	   }
 	}
 
 	//Check for win/draw conditions and update the gamestate
@@ -32,7 +68,7 @@ public class Game
 		{
 			if( player == Symbol.CIRCLE )
 			{
-				gameState = GameState.CIRLCE_WON;
+				gameState = GameState.CIRCLE_WON;
 			}
 			else // (player == Symbol.CROSS)
 			{
