94d2c8bcfe558e5547a07ffa2a854963485110e7
Gudni <gudnig10@ru.is>
2013-11-21 13:05:10 +0000

    added ajax call

--- src/main/resources/public/tictactoe.js
+++ src/main/resources/public/tictactoe.js
@@ -1,6 +1,16 @@
-$(document).ready(function() {
+k$(document).ready(function() {
 	$("#tictactoe tr td").click(function ()
 	{
+		var cell = $(this).attr( 'id' );
+		$.ajax(
+		{
+			type: GET,
+			url: "/play",
+			data: 'cell=' + cell
+        }).done( function( data ) 
+		{
+			$('#cell').html( data );
+        }).
 		alert("SUCCESS");
 	});
   });
