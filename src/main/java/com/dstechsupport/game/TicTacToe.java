5169fb09d1c4dbc811b377f7a9c8437852e540e1
Gudni <gudnig10@ru.is>
2013-11-21 10:37:59 +0000

    added more spark code to test

--- src/main/java/com/dstechsupport/game/TicTacToe.java
+++ src/main/java/com/dstechsupport/game/TicTacToe.java
@@ -12,17 +12,31 @@ public class TicTacToe
 {
     public static void main( String[] args )
     {
+
+
+        setPort(Integer.valueOf(System.getenv("PORT")));
+
         get(new Route("/hello") {
                 @Override
                 public Object handle(Request request, Response response) {
                 return "Hello World!";
                 }
         });
-        System.out.println( "Welcome to this game of Tic Tac Toe!" );
 
-        Game game = new Game();
+	
+        post(new Route("/add") {
+            	@Override
+            	public Object handle(Request request, Response response) {
+                	Integer a = Integer.valueOf(request.queryParams("a"));
+                	Integer b = Integer.valueOf(request.queryParams("b"));
+                	return a + b;
+            	}
+        });
+       // System.out.println( "Welcome to this game of Tic Tac Toe!" );
 
-        game.play();
+        //Game game = new Game();
+
+        //game.play();
 
 
     }
