eaa5b508d02977ce08141a4792bb21d99372e8b2
Gudni <gudnig10@ru.is>
2013-11-21 16:00:29 +0000

    added unit tests for helper functions findRow and findColumn

--- src/test/java/com/dstechsupport/game/TicTacToeTest.java
+++ src/test/java/com/dstechsupport/game/TicTacToeTest.java
@@ -10,8 +10,89 @@ import junit.framework.TestSuite;
 public class TicTacToeTest
     extends TestCase
 {
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public void testFindRow()
+	{
+		TicTacToe tester = new TicTacToe();
+
+		int result = tester.findRow( 1 );
+
+		assertEquals( "this is in row 1", 1, result );
+	}
+
+	public void testFindRow2()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findRow( 5 );
+
+                assertEquals( "this is in row 2", 2, result );
+        }
+
+
+	public void testFindRow3()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findRow( 9 );
+
+                assertEquals( "this is in row 3", 3, result );
+        }
+
+
+	public void testFindRow4()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findRow( 6 );
+
+                assertEquals( "this is in row 2", 2, result );
+        }
+
+	public void testFindColumn()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findColumn( 6 );
+
+                assertEquals( "this is in column 3", 3, result );
+        }
+
+        public void testFindColumn2()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findColumn( 7 );
+
+                assertEquals( "this is in column 1", 1, result );
+        }
+
+        public void testFindColumn3()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findColumn( 9 );
+
+                assertEquals( "this is in column 3", 3, result );
+        }
+
+        public void testFindColumn4()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findColumn( 2 );
+
+                assertEquals( "this is in column 2", 2, result );
+        }
+
+        public void testFindColumn5()
+        {
+                TicTacToe tester = new TicTacToe();
+
+                int result = tester.findColumn( 1 );
+
+                assertEquals( "this is in column 1", 1, result );
+        }
+
+
+
 }
