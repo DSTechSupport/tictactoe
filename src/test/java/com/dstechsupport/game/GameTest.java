820fe84d7d6c30915b6a0299ec1ef9d8af6f826b
Gudni <gudnig10@ru.is>
2013-11-22 14:35:27 +0000

    unit tests for Game.makeMove()

--- src/test/java/com/dstechsupport/game/GameTest.java
+++ src/test/java/com/dstechsupport/game/GameTest.java
@@ -7,13 +7,73 @@
 
 package com.dstechsupport.game;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class GameTest extends TestCase
 {
-	
+
+	/*public void testPlay()
+	{
+		Game lol = new Game();
+		JSONObject result = new JSONObject();
+		result  = lol.Play( 2, 2 );
+
+		String currentGameState =  result.getString( "gameState" );
+		String currentPlayer = result.getString( "currentPlayer" );
+		String success = result.getString( "success" );
+
+		//checking the player switched
+		assertEquals( "after 1 turn current player should be CIRCLE", "CIRCLE", currentPlayer  );
+		assertEquals( "after 1 turn current player should be CIRCLE", Symbol.CIRCLE, tester.currentPlayer );
+
+		//gamestate should be playing after only 1 turn
+		assertEquals( "gamestate should be PLAYING", "PLAYING", currentGameState );
+		assertEquals( "gamestate should be PLAYING", GameState.PLAYING, tester.gameState );
+
+		//success should be true if the play was successful
+		assertEquals( "result should be True", "true", success );
+
+	}*/
+	// tests wrong player making a move
+	public void testMakeMove()
+	{
+		Game tester = new Game();
+
+		boolean result = tester.makeMove( Symbol.CIRCLE, 2, 2 );
+
+		assertFalse( result  );
+	}
+	//tests current player making a move
+        public void testMakeMove2()
+        {
+                Game tester = new Game();
+
+                boolean result = tester.makeMove( Symbol.CROSS, 2, 2 );
+
+                assertTrue( result );
+		assertEquals( "CROSS should now be in the middle cell", Symbol.CROSS, tester.board.cells[1][1].content );
+        }
+
+	//tests making a move to a cell containing a non empty symbol
+	public void testMakeMove3()
+	{
+		Game tester = new Game();
+		tester.board.cells[0][0].content = Symbol.CROSS;
+		tester.currentPlayer = Symbol.CIRCLE;
+
+		boolean result = tester.makeMove( Symbol.CIRCLE, 1, 1 );
+
+		assertFalse( result );
+		assertEquals( " the top left cell should still be CROSS", Symbol.CROSS, tester.board.cells[0][0] );
+	}
+
+
+
 	public void testGame()
 	{
 		Game tester = new Game();
