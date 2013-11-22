7fe772ccf5edaba223cba38870cecfa0a86c4809
Gudni <gudnig10@ru.is>
2013-11-22 21:54:15 +0000

    changes to js

--- src/main/resources/public/tictactoe.js
+++ src/main/resources/public/tictactoe.js
@@ -4,43 +4,63 @@ var currentPlayer = "X";
 $(document).ready(function() {
 	$("#tictactoe tr td").click(function ()
 	{
-		var cell = this
-		var cellid =  $(this).attr( 'id' );
-		$.ajax({
-       	   	type: "post",
-          	url: "/play",
-          	data: 'cell=' + cellid
-        	}).done( function( data ) {
-			alert( data );
-			var results = data.split(",");
-
-			//if the move happened
-			//draw the current player
-			if( results[0] == 'done' )
-			{
-				console.log( "filling cell" );
-				$( cell ).children().html( currentPlayer );
-				currentPlayer = results[1];
+		if( gameState == "playing"  )
+		{
+			var cell = this
+			var cellid =  $(this).attr( 'id' );
+			$.ajax({
+	       	   	type: "post",
+	          	url: "/play",
+	          	data: 'cell=' + cellid
+	        	}).done( function( data ) {
+				alert( data );
+				var results = data.split(",");
+	
+				//if the move happened
+				//draw the current player
+				if( results[0] == 'done' )
+				{
+					console.log( "filling cell" );
+					$( cell ).children().html( currentPlayer );
+					currentPlayer = results[1];
+				}
+				else
+				{
+					console.log( "illegal move" );
+				}
+	
+				if( results[3] != "playing" )
+				{
+					if( results[3] == "xwins" )
+					{
+						alert( "CROSS WINS" );
+					}
+					else if( results[3] == "owins" )
+					{
+						alert( "CIRCLE WINS" );
+					}
+					else if( results[3] == "draw" )
+					{
+						alert( "GAME IS DRAWN" );
+					}
+				}
 			}
+			// if the game has ended
 			else
 			{
-				console.log( "illegal move" );
-			}
-
-			if( results[3] != "playing" )
-			{
 				if( results[3] == "xwins" )
-				{
-					alert( "CROSS WINS" );
-				}
-				else if( results[3] == "owins" )
-				{
-					alert( "CIRCLE WINS" );
-				}
-				else if( results[3] == "draw" )
-				{
-					alert( "GAME IS DRAWN" );
-				}
+                                {
+                                        alert( "CROSS WINS" );
+                                }
+                                else if( results[3] == "owins" )
+                                {
+                                        alert( "CIRCLE WINS" );
+                                }
+                                else if( results[3] == "draw" )
+                                {
+                                        alert( "GAME IS DRAWN" );
+                                }
+
 			}
 		});
 		
